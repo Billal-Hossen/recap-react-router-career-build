dc67aea42c1b05cd2815cb4e5979f21e6fdc86dc
Billal-Hossen <mddipu867@gmail.com>
2024-09-28 15:56:32 +0600

    basic setup ready for react router

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AppliedJobs() {
+  return (
+    <div>
+      <h2>Thsis is applied job page</h2>
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import './index.css'
+import Root from './components/Root/Root.jsx';
+
+import Home from './components/Home/Home.jsx';
+import ErrorPage from './components/error/error-page.jsx';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
